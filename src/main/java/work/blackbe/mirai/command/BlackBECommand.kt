e68810e2e67520f686a281ad396392011823444b
McPlus <mcplusapp@gmail.com>
2023-01-20 16:50:18 +0800

    feat: 新增清除缓存命令
    usage: blackbe cache clear

--- src/main/java/work/blackbe/mirai/command/BlackBECommand.kt
+++ src/main/java/work/blackbe/mirai/command/BlackBECommand.kt
@@ -6,13 +6,23 @@ import net.mamoe.mirai.console.command.CommandContext
 import net.mamoe.mirai.console.command.CompositeCommand
 import net.mamoe.mirai.console.command.ConsoleCommandSender
 import net.mamoe.mirai.console.plugin.jvm.reloadPluginConfig
+import work.blackbe.mirai.config.Global
 
-object ReloadConfig : CompositeCommand(BlackBE.INSTANCE, "blackbe") {
+object BlackBECommand : CompositeCommand(BlackBE.INSTANCE, "blackbe") {
     @SubCommand("reload")
     suspend fun reload(context: CommandContext) {
         if (context.sender is ConsoleCommandSender) {
             BlackBE.INSTANCE.reloadPluginConfig(Config)
-            context.sender.sendMessage("重载配置文件成功")
+            BlackBE.INSTANCE.logger.info("重载配置文件成功")
+        }
+    }
+
+    @SubCommand("cache")
+    suspend fun cache(context: CommandContext, arg: String) {
+        if (arg == "clear") {
+            Global.RESPONSE_CACHE.clear()
+            Global.REPOSITORIES_RESPONSE_CACHE.clear()
+            BlackBE.INSTANCE.logger.info("清除缓存成功")
         }
     }
 }
